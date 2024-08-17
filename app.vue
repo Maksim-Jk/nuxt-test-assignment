418525ba9f79b00673f1dc12d9fe5ac86f6e4f2e
Maksim-Zhuikov <maksim.zhuikov@axtechnology.uz>
2024-08-17 16:18:34 +0500

    feat: add fonts, styles, config

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
+<script lang="ts" setup>
+</script>
+
+<style lang="scss">
+@import 'assets/styles/index.scss';
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const router = useRouter()
+
+router.push('/profile')
+</script>
+
+<template>
+  <div>
+    <h1>Home</h1>
+  </div>
+</template>
